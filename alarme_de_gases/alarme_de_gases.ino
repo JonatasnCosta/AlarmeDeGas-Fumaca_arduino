b5fcb21e39a337637f27e56177362e249db98fb2
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-11-07 06:21:06 -0300

    adicionado o teste e biblio

--- alarme_de_gases/alarme_de_gases.ino
+++ alarme_de_gases/alarme_de_gases.ino
@@ -1,22 +1,44 @@
-#define ledeVerde 2
-#define lampada 6
-#define entradaDigital 13
-#define  entradaAnalogica 0
+// INCLUSÃO DE BIBLIOTECAS
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// INCLUSÃO DE BIBLIOTECAS
+#define sirene 6
+#define entradaDigital 8
+#define entradaAnalogica 0
 #define espera 1000
+#define endereco  0x3F // Endereços comuns: 0x27, 0x3F
+#define colunas  16
+#define linhas   2
 
 bool dSensor;
 int aSensor;
 
-void setup() {
+// INSTANCIANDO OBJETOS
+LiquidCrystal_I2C lcd(endereco, colunas, linhas);
 
+void setup() {
+  lcd.init(); // INICIA A COMUNICAÇÃO COM O DISPLAY
+  lcd.backlight(); // LIGA A ILUMINAÇÃO DO DISPLAY
+  lcd.clear(); // LIMPA O DISPLAY
+
+  lcd.print("Iniciando ...");
+  delay(5000); // DELAY DE 5 SEGUNDOS
+  lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
+  lcd.print("Sensor de Gas");
+  delay(5000); // DELAY DE 5 SEGUNDOS
+  
+  lcd.noBacklight(); // DESLIGA A ILUMINAÇÃO DO DISPLAY
+  delay(2000); // DELAY DE 2 SEGUNDOS
+  lcd.backlight(); // LIGA A ILUMINAÇÃO DO DISPLAY
+  delay(2000); // DELAY DE 2 SEGUNDOS
+  
+  lcd.clear(); // LIMPA O DISPLAY
+  //lcd.noBacklight(); // DESLIGA A ILUMINAÇÃO DO DISPLAY
   Serial.begin(9600);
   pinMode(entradaDigital, INPUT);
- 
-  pinMode(ledeVerde, OUTPUT);
-  
-  
-  pinMode(lampada, OUTPUT);
-  digitalWrite(lampada, HIGH ); 
+  pinMode(sirene, OUTPUT);
+  digitalWrite(sirene, HIGH ); 
 
 }
 
@@ -25,30 +47,21 @@ void loop() {
   dSensor = digitalRead (entradaDigital);
   aSensor = analogRead (entradaAnalogica);
 
-  Serial.print("Leitura entrada digital:");
-  Serial.println(dSensor);
 
-  Serial.print("Nível do Gás:");
-  Serial.println(aSensor);
-  Serial.println();
-  delay(1000);
-
-  if (aSensor < 100)
-  {
-   digitalWrite(ledeVerde, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledeVerde, LOW);
-  }
   
- 
    if (aSensor >= 100)
   {
-   digitalWrite(lampada, LOW );
+   digitalWrite(sirene, LOW );
   }
   else
   {
-     digitalWrite(lampada, HIGH );
+    digitalWrite(sirene, HIGH );
   }
+  lcd.print("Nivel do Gas:");
+  lcd.setCursor(0,1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
+  lcd.print(aSensor);
+  lcd.setCursor(0,2);
+  delay(1000);
+  lcd.clear();
+ 
 }
